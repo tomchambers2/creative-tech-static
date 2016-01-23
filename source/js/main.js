ce6d89bc1df9727787673b2ef19ef5753c51362c
Tom Chambers <tom.chambers@gmail.com>
2016-01-23 15:15:30 +0000

    videos only play when seen

--- source/js/main.js
+++ source/js/main.js
@@ -6,10 +6,6 @@ function setVideoHeight() {
 
 $(document).ready(function(){
 	$('.carousel-container').on('init', function() {
-		$('.carousel-video').each(function() {
-			this.play();
-		})
-
 		setVideoHeight();
 	});
 
@@ -21,11 +17,17 @@ $(document).ready(function(){
 		autoplay: true,
 		accessibility: true,
 		dots: true,
-		autoplaySpeed: 5000
+		autoplaySpeed: 1000
 	});
 
-	$('.carousel-container').on('afterChange', function() {
-	
+	$('.carousel-container').on('afterChange', function(e, slick, index) {
+		var video = $(this).find('*[data-slick-index='+index+'] video').get()[0];
+		video.play();
+		$('.carousel-container').slick('slickPause');
+		video.addEventListener('ended', function() {
+			$('.carousel-container').slick('slickPlay');
+			video.pause();
+		});
 	});
 
 	$('.grid').imagesLoaded( function(){
