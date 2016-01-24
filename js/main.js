1608c380f5a3bb1ec33e6810ee1eccaab43d0421
Tom Chambers <tom.chambers@gmail.com>
2016-01-24 15:13:49 +0000

    Update 2016-01-24T15:13:46.034Z

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function setVideoHeight() {
 }
 
 $(document).ready(function(){
+	var currentSlide;
+
 	$('.carousel-container').on('init', function() {
 		setVideoHeight();
 	});
@@ -20,8 +22,19 @@ $(document).ready(function(){
 		autoplaySpeed: 1000
 	});
 
+	$(window).on('scroll', function() {
+		var video = $('.carousel-container').find('*[data-slick-index='+currentSlide+'] video').get()[0];
+		if ($(window).scrollTop() >= $(window).height()) {
+			video.pause();	
+		} else {
+			video.play();
+		}
+	});
+
 	$('.carousel-container').on('afterChange', function(e, slick, index) {
+		currentSlide = index;
 		var video = $(this).find('*[data-slick-index='+index+'] video').get()[0];
+		if (!video) return;
 		video.play();
 		$('.carousel-container').slick('slickPause');
 		video.addEventListener('ended', function() {
