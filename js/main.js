719d7e28c21d6953e617de3665c5b32cf65fc53e
Tom Chambers <tom.chambers@gmail.com>
2016-02-10 21:44:47 +0000

    Update 2016-02-10T21:44:23.495Z

--- js/main.js
+++ js/main.js
@@ -1,46 +1,23 @@
 function setVideoHeight() {
-	var offset = $('.carousel-video').height() - $('.carousel-item').height()
+	console.log('setting height')
+	var offset = $('.showreel-container video').height() - $('.showreel-container').height()
 	console.log(offset)
-	$('.carousel-video').css({ bottom: offset });
+	$('#showreel-video').css({ bottom: offset, position: 'relative' });
 }
 
 $(document).ready(function(){
-	var currentSlide;
-
-	$('.carousel-container').on('init', function() {
-		setVideoHeight();
-	});
-
 	$(window).resize(function() {
 		setVideoHeight();
 	});
-
-	$('.carousel-container').slick({
-		autoplay: true,
-		accessibility: true,
-		dots: true,
-		autoplaySpeed: 1000
-	});
+	setVideoHeight();
 
 	$(window).on('scroll', function() {
-		var video = $('.carousel-container').find('*[data-slick-index='+currentSlide+'] video').get()[0];
+		var video = document.getElementById('showreel-video');
 		if ($(window).scrollTop() >= $(window).height()) {
 			video.pause();	
 		} else {
 			video.play();
 		}
-	});
-
-	$('.carousel-container').on('afterChange', function(e, slick, index) {
-		currentSlide = index;
-		var video = $(this).find('*[data-slick-index='+index+'] video').get()[0];
-		if (!video) return;
-		video.play();
-		$('.carousel-container').slick('slickPause');
-		video.addEventListener('ended', function() {
-			$('.carousel-container').slick('slickPlay');
-			video.pause();
-		});
 	});
 
 	$('.grid').imagesLoaded( function(){
